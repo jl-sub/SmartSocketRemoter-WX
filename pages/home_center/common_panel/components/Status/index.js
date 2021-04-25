cbec9b18f395e3671c002cf5698eec2e48ffed8d
jl-sub <jl2k@qq.com>
2021-04-25 17:30:46 +0800

    Initial Commit

---
+++ pages/home_center/common_panel/components/Status/index.js
@@ -0,0 +1,26 @@
+// pages/home_center/common_panel/components/Status/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    dpStatus: String,
+    dpUnit: String,
+    dpIcon: String,
+    dpName: String
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
